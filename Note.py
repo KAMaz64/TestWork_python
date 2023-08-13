5be7bec38eb89f8bcf1ad5eec62a31bd4c9fd4cb
KAMaz64 <konstantin.a.mazunin@gmail.com>
2023-08-13 03:35:26 +0300

    Test1_core parts done

---
+++ Note.py
@@ -0,0 +1,9 @@
+
+
+class Note:
+    def __init__(self, note_id, title, body, creation_date, last_modified):
+        self.note_id = note_id
+        self.title = title
+        self.body = body
+        self.creation_date = creation_date
+        self.last_modified = last_modified
---
+++ Task1_main.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+from NoteManager import NoteManager
+
+def main():
+    note_manager = NoteManager()
+
+    while True:
+        print("\nВыберите действие:")
+        print("1. Добавить новую заметку")
+        print("2. Просмотреть список заметок по дате")
+        print("3. Просмотреть список всех заметок")
+        print("4. Редактировать заметку")
+        print("5. Удалить заметку")
+        print("6. Выйти из программы")
+
+        choice = input("Введите номер действия: ")
+
+        if choice == "1":
+            title = input("Введите заголовок заметки: ")
+            body = input("Введите текст заметки: ")
+            note_manager.add_note(title, body)
+        elif choice == "2":
+            date_str = input("Введите дату (гггг-мм-дд), для просмотра заметок этой даты: ")
+            try:
+                target_date = datetime.strptime(date_str, "%Y-%m-%d")
+                note_manager.view_notes_by_date(target_date)
+            except ValueError:
+                print("Некорректный формат даты. Введите дату в формате гггг-мм-дд.")
+        elif choice == "3":
+            note_manager.view_all_notes()
+        elif choice == "4":
+            note_id = int(input("Введите ID заметки, которую хотите отредактировать: "))
+            new_title = input("Введите новый заголовок: ")
+            new_body = input("Введите новый текст: ")
+            note_manager.edit_note(note_id, new_title, new_body)
+        elif choice == "5":
+            note_id = int(input("Введите ID заметки, которую хотите удалить: "))
+            note_manager.delete_note(note_id)
+        elif choice == "6":
+            print("Программа завершена.")
+            break
+        else:
+            print("Некорректный выбор. Введите номер действия снова.")
+
+if __name__ == "__main__":
+    main()
